4f1c4c5035b5a74be6592e84a45d545932cf1eef
bigmanhandles <56513089+Auraaaaaaaa@users.noreply.github.com>
2022-09-14 11:07:43 +0100

    k

---
+++ actual code O-O/Subroutines.py
@@ -0,0 +1,10 @@
+def average_value(a,b,c):
+    return((a+b+c)/3)
+print("fun calculator of doom")
+a = input("Enter a number: ")
+b = input("Enter a number: ")
+c = input("Enter a number: ")
+a = int(a)
+b = int(b)
+c = int(c)
+print("The average of the three numbers is: ", average_value(a,b,c))
---
+++ actual code O-O/Times table generator.py
@@ -0,0 +1,6 @@
+times_table = 10
+answer = 0
+print(f"Here is the {times_table} times table")
+for x in range(1,13):
+    answer = x * times_table
+    print(f"{x} times {times_table} is {answer}")

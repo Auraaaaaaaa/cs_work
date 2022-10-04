f83397e52818020f163928da03f42eaddbcdf113
bigmanhandles <56513089+Auraaaaaaaa@users.noreply.github.com>
2022-10-04 19:38:13 +0100

    balls

--- actual code O-O/Ticketing/Ticketing system scenario.py
+++ actual code O-O/Ticketing/Ticketing system scenario.py
@@ -1,5 +1,10 @@
 import datetime
 import os
+from turtle import st
+from reportlab.platypus import SimpleDocTemplate, Table, Paragraph, TableStyle
+from reportlab.lib import colors
+from reportlab.lib.pagesizes import A4
+from reportlab.lib.styles import getSampleStyleSheet
 def clear():
    if os.name == 'posix' or 'Darwin':
       _ = os.system('clear')
@@ -23,6 +28,23 @@ print("Your total is £" + str(total))
 print("I am afraid that you need to pay with cash, in £10 or £20 notes")
 notes_10 = int(input("How many £10 notes do you have? "))
 notes_20 = int(input("How many £20 notes do you have? "))
+def generate_receipt():
+    doc = SimpleDocTemplate("receipt.pdf", pagesize=A4)
+    elements = []
+    data = [
+        ["Surname", "Adult", "Child", "Senior", "Wristband", "Parking", "Total"],
+        [surname_for_booking, adult_tickets, child_tickets, senior_tickets, wristband_tickets, parking_pass, total]
+    ]
+    t = Table(data)
+    t.setStyle(TableStyle(
+        [
+            ('GRID', (0, 0), (-1, -1), 1, colors.black),
+            ('BACKGROUND', (0, 0), (-1, 0), colors.lightgrey),
+            ('BACKGROUND', (0, 1), (-1, -1), colors.white),
+        ]
+    ))
+    elements.append(t)
+    doc.build(elements)
 if (notes_10 * 10) + (notes_20 * 20) < total:
     print("You do not have enough money")
     exit(0)
@@ -39,19 +61,6 @@ else:
     print("Total: £" + str(total))
     print("Change: £" + str((notes_10 * 10) + (notes_20 * 20) - total))
     print("Have a nice day!")
-    print("Your tickets (and parking pass) will be printed now\n")
-    print("+------------------------+")
-    print("Name: " + surname_for_booking)
-    print("Adult tickets: " + str(adult_tickets))
-    print("Child tickets: " + str(child_tickets))
-    print("Senior tickets: " + str(senior_tickets))
-    print("Wristband tickets: " + str(wristband_tickets))
-    print("Total: £" + str(total))
-    print("Day of purchase: " + str(datetime.datetime.now()))
-    print("+------------------------+")
-    print("Parking pass:")
-    print("+------------------------+")
-    print("Parking Pass")
-    print("Name: " + surname_for_booking)
-    print("Day of purchase: " + str(datetime.datetime.now()))
-    print("+------------------------+")
+    print("Your tickets will be printed now\n")
+    generate_receipt()
+    print("Your receipt is in 'receipt.pdf'")
